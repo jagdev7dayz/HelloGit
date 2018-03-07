5f001fe2fe04e2a4552b5bffb85b87879aa8fa2b
Jagdev Singh Sendhav <jagdev.sendhav@yash.com>
2018-03-07 17:30:25 +0530

    Initial commit

---
+++ HelloAPP/HelloAPP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloAPP
+//
+//  Created by Jagdev on 3/7/18.
+//  Copyright © 2018 Dev2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
